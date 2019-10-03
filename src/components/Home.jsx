106c8d6193c2bfe7ff274194a81ef95af32349a7
Florian-Ch <gassenbube@gmail.com>
2019-10-03 14:49:42 +0200

    changement de salle

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,12 +3,24 @@ import SearchBar from './SearchBar'
 import Button from './Button'
 
 export default class Home extends Component {
+  constructor(props){
+    super(props)
+
+    var switch_sort = React.createRef()
+
+  }
+
+  onClickButton(){
+    console.log()
+  }
+
   render() {
     return (
       <div class="container">
         <div class="raw"> {/* ligne de titre */}
           <h1>Client Jeux de mots</h1>
         </div>
+
         <div class="raw"> {/* ligne des options */}
           {/*<Button name="test"/>*/}
           <div class="input-group input-group-sm mb-3">
@@ -17,7 +29,13 @@ export default class Home extends Component {
             </div>
             <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" style={{maxWidth:"50px"}}/>
           </div>
+          <div class="btn-group-toggle" data-toggle="buttons">
+            <label class="btn btn-secondary active">
+              <input type="checkbox" checked autocomplete="off"/> Tri par poids
+            </label>
+          </div>
         </div>
+
         <div class="raw"> {/* ligne de barre de recherche */}
           <SearchBar/>
         </div>
